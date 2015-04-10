432fa134cdfff061d08745c0ee914e385cf91038
hasit <hasit@uw.edu>
2015-04-10 11:03:07 -0700

    init code structure; just a start

--- licensethis.go
+++ licensethis.go
@@ -1,7 +1,24 @@
+// Package main adds an OSS license of choice to the current directory
 package main
 
-func main() {
-	if readFlags() != nil {
+import "fmt"
 
+// Usage prints the proper usage of licensethis from godoc.org
+func Usage() {
+	fmt.Println("Usage of licensethis:")
+	return
+}
+
+// ParseFlags parses command-line flags, checks for valid flags and stores the values in a global structure for further use
+func ParseFlags() error {
+	// Parse flags here
+	var e error
+	return e
+}
+
+func main() {
+	if ParseFlags() != nil {
+		Usage()
+		return
 	}
 }

80f94896c175666a07fab2493c172852fec26bcd
hasit <hasit@uw.edu>
2015-04-12 14:15:45 -0700

    man files added

--- licensethis.go
+++ licensethis.go
@@ -1,4 +1,3 @@
-// Package main adds an OSS license of choice to the current directory
 package main
 
 import (
@@ -6,9 +5,11 @@ import (
 	"os"
 )
 
-func main() {
-	args := os.Args[1:]
+func printHelp() {
 
+}
+
+func parseArgs(args []string) {
 	if len(args) != 0 {
 		switch args[0] {
 		case "help":
@@ -17,12 +18,22 @@ func main() {
 			fmt.Println("generate")
 		case "list":
 			fmt.Println("list")
+		case "info":
+			fmt.Println("info")
 		case "config":
 			fmt.Println("config")
 		default:
-			fmt.Println("help")
+			fmt.Println("Incorrect usage!")
+			fmt.Println("Type `licensethis help` for help on proper usage.")
 		}
 	} else {
-		fmt.Println("help")
+		fmt.Println("Incorrect usage!")
+		fmt.Println("Type `licensethis help` for help on proper usage.")
 	}
+}
+
+func main() {
+	args := os.Args[1:]
+
+	parseArgs(args)
 }
